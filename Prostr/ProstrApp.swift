e3b78ad9c54a6324f4616818c322b3997d5b1dd9
Alex Bezkopylnyi <sanok.bes@gmail.com>
2026-03-20 17:45:29 +0000

    Initial Commit

---
+++ Prostr/ProstrApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProstrApp.swift
+//  Prostr
+//
+//  Created by AlexBezkopylnyi on 20.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct ProstrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProstrTests/ProstrTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProstrTests.swift
+//  ProstrTests
+//
+//  Created by AlexBezkopylnyi on 20.03.2026.
+//
+
+import Testing
+@testable import Prostr
+
+struct ProstrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
